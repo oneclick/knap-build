1267141fc761276ab1cdcbc3363c7e6267d271be
Luis Lavena <luislavena@gmail.com>
2012-01-22 20:47:21 -0300

    Can extract now

--- lib/knapsack/utils.rb
+++ lib/knapsack/utils.rb
@@ -1,13 +1,14 @@
+require "digest/md5"
 require "fileutils"
 
 module Knapsack
   module Utils
-    def download(url, target)
-      return true if File.exists?(target)
+    def download(url, filename)
+      return true if File.exists?(filename)
 
-      ensure_tree target
+      ensure_tree File.dirname(filename)
 
-      cmd = ["curl", "-s", "-S", url, "-o", target]
+      cmd = ["curl", "-s", "-S", url, "-o", filename]
 
       pid = Process.spawn(*cmd, :err => :out, :out => IO::NULL)
       _, status = Process.wait2(pid)
@@ -17,12 +18,25 @@ module Knapsack
     module_function :download
 
     def extract(filename, md5, target)
+      ensure_tree target
+
+      # verify checksum first
+      computed_md5 = Digest::MD5.file(filename).hexdigest
+      unless computed_md5 == md5
+        raise "MD5 verification failed for #{File.basename(filename)} (expected: #{md5}, was: #{computed_md5}"
+      end
+
+      cmd = ["tar", "xf", filename, "-C", target]
+
+      pid = Process.spawn(*cmd, :err => :out, :out => IO::NULL)
+      _, status = Process.wait2(pid)
+
+      status.success?
     end
     module_function :extract
 
-    def ensure_tree(filename)
-      dirname = File.dirname(filename)
-      FileUtils.mkdir_p dirname
+    def ensure_tree(target)
+      FileUtils.mkdir_p target
     end
     module_function :ensure_tree
   end
