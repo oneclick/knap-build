bc495f54df1fe2f3ced6e053093e8e8592688547
Luis Lavena <luislavena@gmail.com>
2012-02-01 19:52:34 -0300

    Basic option parsing for knap builder
    Takes verbose and platform options

--- bin/knap.rb
+++ bin/knap.rb
@@ -1,6 +1,7 @@
 # encoding: UTF-8
 
 require "knapsack"
+require "optparse"
 
 files = Dir.glob("#{Knapsack.var_root}/recipes/**/*.knapfile").sort
 
@@ -8,12 +9,33 @@ files.each do |f|
   Knapsack::Recipe.add_recipe(f)
 end
 
+options = {}
+opts = OptionParser.new do |opts|
+  opts.on("-p", "--platform PLATFORM", "Specify the target platform") do |v|
+    options[:platform] = v
+  end
+
+  opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+    options[:verbose] = v
+  end
+end
+opts.parse!(ARGV)
+
 recipe_name = ARGV.pop
 recipe = Knapsack::Recipe.find_by_name recipe_name
 
 exit if defined?(Exerb)
 
 if recipe
+  # set options
+  if options.has_key?(:verbose)
+    recipe.options.verbose = options[:verbose]
+  end
+
+  if options.has_key?(:platform)
+    recipe.platform = options[:platform]
+  end
+
   if recipe.pending?
     recipe.cook
   end
